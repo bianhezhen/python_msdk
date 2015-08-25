4a115d21162fa942bcea87a8997aa214c6e92676
bianhezhen <bianhezhen@163.com>
2015-08-25 21:27:58 +0800

    Update msdk.py

--- msdk.py
+++ msdk.py
@@ -11,3 +11,18 @@ def verify_login(sdk, params, qs):
     method = 'POST'
     url_path = '/auth/verify_login'
     return sdk.api_msdk(url_path, params, qs, method)
+
+
+def load_vip(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.1_/profile/load_vip
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> load_vip(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/load_vip'
+    return sdk.api_msdk(url_path, params, qs, method)
