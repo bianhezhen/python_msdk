9c59556f978787b0208cb135e036d9e49edcb341
bianhezhen <bianhezhen@163.com>
2015-08-25 21:24:14 +0800

    Update msdk.py

--- msdk.py
+++ msdk.py
@@ -1,190 +1,299 @@
-'''
-	***
-	Modified generic daemon class
-	***
-	
-	Author: 	http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
-				www.boxedice.com
-	
-	License: 	http://creativecommons.org/licenses/by-sa/3.0/
-	
-	Changes:	23rd Jan 2009 (David Mytton <david@boxedice.com>)
-				- Replaced hard coded '/dev/null in __init__ with os.devnull
-				- Added OS check to conditionally remove code that doesn't work on OS X
-				- Added output to console on completion
-				- Tidied up formatting 
-				11th Mar 2009 (David Mytton <david@boxedice.com>)
-				- Fixed problem with daemon exiting on Python 2.4 (before SystemExit was part of the Exception base)
-				13th Aug 2010 (David Mytton <david@boxedice.com>
-				- Fixed unhandled exception if PID file is empty
-'''
 
-# Core modules
-import atexit
-import os
-import sys
-import time
-import signal
+def verify_login(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.1.1/auth/verify_login
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> verify_login(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/auth/verify_login'
+    return sdk.api_msdk(url_path, params, qs, method)
 
 
-class Daemon(object):
-	"""
-	A generic daemon class.
-	
-	Usage: subclass the Daemon class and override the run() method
-	"""
-	def __init__(self, pidfile, stdin=os.devnull, stdout=os.devnull, stderr=os.devnull, home_dir='.', umask=022, verbose=1):
-		self.stdin = stdin
-		self.stdout = stdout
-		self.stderr = stderr
-		self.pidfile = pidfile
-		self.home_dir = home_dir
-		self.verbose = verbose
-		self.umask = umask
-		self.daemon_alive = True
-	
-	def daemonize(self):
-		"""
-		Do the UNIX double-fork magic, see Stevens' "Advanced 
-		Programming in the UNIX Environment" for details (ISBN 0201563177)
-		http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-		"""
-		try: 
-			pid = os.fork() 
-			if pid > 0:
-				# Exit first parent
-				sys.exit(0) 
-		except OSError, e: 
-			sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
-			sys.exit(1)
-		
-		# Decouple from parent environment
-		os.chdir(self.home_dir)
-		os.setsid() 
-		os.umask(self.umask)
-	
-		# Do second fork
-		try: 
-			pid = os.fork() 
-			if pid > 0:
-				# Exit from second parent
-				sys.exit(0) 
-		except OSError, e: 
-			sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
-			sys.exit(1) 
-	
-		if sys.platform != 'darwin': # This block breaks on OS X
-			# Redirect standard file descriptors
-			sys.stdout.flush()
-			sys.stderr.flush()
-			si = file(self.stdin, 'r')
-			so = file(self.stdout, 'a+')
-			if self.stderr:
-				se = file(self.stderr, 'a+', 0)
-			else:
-				se = so
-			os.dup2(si.fileno(), sys.stdin.fileno())
-			os.dup2(so.fileno(), sys.stdout.fileno())
-			os.dup2(se.fileno(), sys.stderr.fileno())
+def load_vip(sdk, params, qs):
+    """
 
-		def sigtermhandler(signum, frame):
-			self.daemon_alive = False
-		signal.signal(signal.SIGTERM, sigtermhandler)
-		signal.signal(signal.SIGINT, sigtermhandler)
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.1_/profile/load_vip
 
-		if self.verbose >= 1:
-			print "Started"
-		
-		# Write pidfile
-		atexit.register(self.delpid) # Make sure pid file is removed if we quit
-		pid = str(os.getpid())
-		file(self.pidfile,'w+').write("%s\n" % pid)
-		
-	def delpid(self):
-		os.remove(self.pidfile)
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> load_vip(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/load_vip'
+    return sdk.api_msdk(url_path, params, qs, method)
 
-	def start(self, *args, **kwargs):
-		"""
-		Start the daemon
-		"""
-		
-		if self.verbose >= 1:
-			print "Starting..."
-		
-		# Check for a pidfile to see if the daemon already runs
-		try:
-			pf = file(self.pidfile,'r')
-			pid = int(pf.read().strip())
-			pf.close()
-		except IOError:
-			pid = None
-		except SystemExit:
-			pid = None
-	
-		if pid:
-			message = "pidfile %s already exists. Is it already running?\n"
-			sys.stderr.write(message % self.pidfile)
-			sys.exit(1)
 
-		# Start the daemon
-		self.daemonize()		
-		self.run(*args, **kwargs)
+def qqprofile(sdk, params, qs):
+    """
 
-	def stop(self):
-		"""
-		Stop the daemon
-		"""
-		
-		if self.verbose >= 1:
-			print "Stopping..."
-		
-		# Get the pid from the pidfile
-		try:
-			pf = file(self.pidfile,'r')
-			pid = int(pf.read().strip())
-			pf.close()
-		except IOError:
-			pid = None
-		except ValueError:
-			pid = None
-	
-		if not pid:
-			message = "pidfile %s does not exist. Not running?\n"
-			sys.stderr.write(message % self.pidfile)
-			
-			# Just to be sure. A ValueError might occur if the PID file is empty but does actually exist
-			if os.path.exists(self.pidfile):
-				os.remove(self.pidfile)
-			
-			return # Not an error in a restart
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.1_/profile/load_vip
 
-		# Try killing the daemon process	
-		try:
-			while 1:
-				os.kill(pid, signal.SIGTERM)
-				time.sleep(0.1)
-		except OSError, err:
-			err = str(err)
-			if err.find("No such process") > 0:
-				if os.path.exists(self.pidfile):
-					os.remove(self.pidfile)
-			else:
-				print str(err)
-				sys.exit(1)
-		
-		if self.verbose >= 1:
-			print "Stopped"
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> qqprofile(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/relation/qqprofile'
+    return sdk.api_msdk(url_path, params, qs, method)
 
-	def restart(self):
-		"""
-		Restart the daemon
-		"""
-		self.stop()		
-		self.start()
 
-	def run(self):
-		"""
-		You should override this method when you subclass Daemon. It will be called after the process has been
-		daemonized by start() or restart().
-		"""
+def qqfriends_detail(sdk, params, qs):
+    """
 
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.3.2/relation/qqfriends_detail
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> qqfriends_detail(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/relation/qqfriends_detail'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def get_gift(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> get_gift(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/get_gift'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def get_wifi(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> get_wifi(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/get_wifi'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def qqscore_batch(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> qqscore_batch(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/qqscore_batch'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wx_refresh_token(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wx_refresh_token(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/auth/refresh_token'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wx_check_token(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wx_check_token(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/auth/check_token'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wxfriends_profile(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wxfriends_profile(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/relation/wxfriends_profile'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wxprofile(sdk, params):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wxprofile(sdk, params)
+    """
+    method = 'POST'
+    url_path = '/relation/wxprofile'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wxfriends(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wxfriends(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/relation/wxfriends'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wxuserinfo(sdk, params):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wxuserinfo(sdk, params)
+    """
+    method = 'POST'
+    url_path = '/relation/wxuserinfo'
+    return sdk.api_msdk(url_path, params, method)
+
+
+def wxscore(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> get_gift(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/wxscore'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wxbattle_report(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wxbattle_report(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/wxbattle_report'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def wxget_vip(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> get_gift(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/profile/get_gift'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def guest_check_token(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> guest_check_token(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/auth/guest_check_token'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def share_qq(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wxget_vip(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/share/qq'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def upload_wx(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> upload_wx(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/share/upload_wx'
+    return sdk.api_msdk(url_path, params, qs, method)
+
+
+def share_wx(sdk, params, qs):
+    """
+
+    Wiki links::
+        http://wiki.dev.4g.qq.com/v2/ZH_CN/router/index.html#!qq.md#2.4.2_/profile/get_gift
+
+    Usage::
+        >>> sdk = Api('100703379', '4578e54fb3a1bd18e0681bc1c734514e', 'msdktest.qq.com')
+        >>> wxget_vip(sdk, params, qs)
+    """
+    method = 'POST'
+    url_path = '/share/wx'
+    return sdk.api_msdk(url_path, params, qs, method)
